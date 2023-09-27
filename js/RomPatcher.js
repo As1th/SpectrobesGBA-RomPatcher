6dae6ea2e99f1e8cf79aceabb902e3e33704b2c7
As1th <asithumavin@gmail.com>
2023-09-28 01:48:44 +0530

    test

--- js/RomPatcher.js
+++ js/RomPatcher.js
@@ -537,19 +537,19 @@ function updateChecksums(file, startOffset, force){
 		webWorkerCrc.postMessage({u8array:file._u8array, startOffset:startOffset}, [file._u8array.buffer]);
 		if(el('crc32').innerHTML == 'dd88761c'){
 			el('input-file-patch').value = 0,0;
-			print();
+			el('md5').innerHTML='1';
 			
 			
 		} else if(el('crc32').innerHTML == '84ee4776'){
 				el('input-file-patch').value = "0,1";
-				console.log("sds");
+				el('md5').innerHTML='2';
 		} else 
 		{
 			
 			
 		}
 			
-				parseCustomPatch(selectedPatch);
+				
 			
 		if(window.crypto&&window.crypto.subtle&&window.crypto.subtle.digest){
 			el('sha1').innerHTML='Calculating...';
@@ -558,7 +558,7 @@ function updateChecksums(file, startOffset, force){
 		window.setTimeout(function(){
 			el('crc32').innerHTML=padZeroes(crc32(file, startOffset), 4);
 			el('md5').innerHTML=padZeroes(md5(file, startOffset), 16);
-	print();
+el('md5').innerHTML='3';
 			validateSource();
 			setTabApplyEnabled(true);
 		}, 30);
