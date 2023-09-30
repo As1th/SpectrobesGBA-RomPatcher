31fb7b8ec159352cf9beafe1609364addd701d23
As1th <asithumavin@gmail.com>
2023-10-01 02:00:51 +0530

    Error message change

--- js/locale.js
+++ js/locale.js
@@ -23,10 +23,10 @@ const LOCALIZATION={
 		'patch_type':				'Patch type:',
 		'creating_patch':			'Creating patch...',
 
-		'error_crc_input':			'Source ROM checksum mismatch',
+		'error_crc_input':			'Could not identify valid Firered ROM. Please ensure you select a correct Firered GBA ROM. (Source ROM checksum mismatch)',
 		'error_crc_output':			'Target ROM checksum mismatch',
 		'error_crc_patch':			'Patch checksum mismatch',
-		'error_downloading':		'Error downloading %s',
+		'error_downloading':		'Error loading %s',
 		'error_unzipping':			'Error unzipping file',
 		'error_invalid_patch':		'Invalid patch file',
 		'warning_too_big':			'Using big files is not recommended.'
