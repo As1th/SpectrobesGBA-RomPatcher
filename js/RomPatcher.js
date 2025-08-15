9bcf2af089ecc8dc85412e53244fad7a3ca04d5c
As1th <asithumavin@gmail.com>
2025-08-15 10:08:58 +0530

    .save

--- js/RomPatcher.js
+++ js/RomPatcher.js
@@ -761,7 +761,7 @@ function preparePatchedRom(originalRom, patchedRom, headerSize) {
 
 
 	setMessage('apply');
-	
+	patchedRom.save();
 	 // Ensure proper download without .txt extension
     const blob = new Blob([patchedRom.data || patchedRom], { type: patchedRom.fileType });
     const link = document.createElement('a');
